97e83ba393b16301d54558a05849f750ec98a952
alfred1123 <wing3h3@gmail.com>
2023-03-16 15:17:15 +0800

    MA, performace completed

--- utils/price.py
+++ utils/price.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 import math
 from time import time
+from perf import perf
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 1000)
+pd.set_option('display.width', None)
 
-class price():
+class price(perf):
     def __init__(self,df):
         if not isinstance(df,pd.DataFrame):
             raise TypeError("self.df is not a dataframe")
@@ -15,29 +16,44 @@ class price():
         self.df.columns = ["datetime","adjclose"]
 
 
-
     def MA(self, timeframe:int, diff:float, trade_days:int, pos = "l"):
 
         self.df = self.df[self.df.columns[0:2]]
-        self.df.columns = ["datetime","adjclose"]
+        self.df = self.df.rename(columns=dict(zip(df.columns.tolist(),["datetime","adjclose"])))
         self.df['chg'] = self.df.adjclose.pct_change()
         self.df['ma'] = self.df.adjclose.rolling(timeframe).mean()
         
-        def ls_pos(diff):
-            return 1 if self.df.adjclose - self.df.ma > diff else -1 if self.df.col[1] - self.df.ma < -diff else 0
+        def ls_pos(row,diff):
+            print(row)
+            if row.adjclose - row.ma > diff:
+                return 1  
+            elif row.adjclose - row.ma < -diff:
+                return -1
+            else:
+                return 0
             
         if pos == "l":
             self.df['pos'] = np.where(self.df.adjclose - self.df.ma > diff,1,0)
         elif pos == "ls":
-            self.df['pos'] = np.vectorize(ls_pos) 
+            self.df['pos'] = self.df.apply(ls_pos, args = (diff,),axis = 1)
+        #     for i in range(len(df)):
+        #         if df.adjclose - df.ma > diff:
+        #             df.loc[i,'pos'] = 1
+        #         if df.adjclose - df.ma < diff:
+        #             df.loc[i,'pos'] = -1
+        #         else:
+        #             df.loc[i,'pos'] = 0
+                
         else: 
             raise Exception("Invalid pos input")
         
+        super().perf_col(self.df)
         
-        self.df['pos_xt1'] = self.df.pos.shift(1)
-        self.df['pnl'] = self.df.pos_xt1* self.df.chg
-        self.df['cumu'] = self.df.pnl.cumsum()
-        self.df['dd'] = self.df.cumu - self.df.cumu.cummax()
+        
+        # self.df['pos_xt1'] = self.df.pos.shift(1)
+        # self.df['pnl'] = self.df.pos_xt1* self.df.chg
+        # self.df['cumu'] = self.df.pnl.cumsum()
+        # self.df['dd'] = self.df.cumu - self.df.cumu.cummax()
         return self.df
 
 # TR = float(self.df.cumu.tail(1))
