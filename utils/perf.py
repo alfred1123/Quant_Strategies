77fcefffabab19c40a3a865219843426aab77599
alfred1123 <wing3h3@gmail.com>
2023-03-18 12:04:54 +0800

    comments added

--- utils/perf.py
+++ utils/perf.py
@@ -1,27 +1,6 @@
 import pandas as pd
 import numpy as np
 
-class perf:
+
     
-    def perf_col(self,df):
-        df['pos_xt1'] = df.pos.shift(1)
-        df['pnl'] = df.pos_xt1* df.chg
-        df['cumu'] = df.pnl.cumsum()
-        df['dd'] = df.cumu - df.cumu.cummax()
-        
-        
-        
-    def TR(self,df):
-        return float(self.df.cumu.tail(1))
     
-    def AR(self,df):
-        return self.df.pnl.mean()*365
-    
-    def Sharpe(self,df):
-        return self.df.pnl.mean()*365*math.sqrt(trade_days)
-    
-    def MDD(self,df):
-        return abs(min(self.df.dd))
-    
-    def Calmar(self,df):
-        return self.AR(df)/self.MDD(df)
--- utils/price.py
+++ utils/price.py
@@ -2,11 +2,42 @@ import pandas as pd
 import numpy as np
 import math
 from time import time
-from perf import perf
+# from perf import perf
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
+
+class perf:
+    
+    def __init__(self,df) -> None:
+        if not isinstance(df,pd.DataFrame):
+            raise TypeError("self.df is not a dataframe")
+        self.df = df
+    
+    def Perf_col(self):
+        print(self.df)
+        self.df['pos_xt1'] = self.df.pos.shift(1)
+        self.df['pnl'] = self.df.pos_xt1* self.df.chg
+        self.df['cumu'] = self.df.pnl.cumsum()
+        self.df['dd'] = self.df.cumu - self.df.cumu.cummax()
+        
+        
+        
+    def TR(self):
+        return float(self.df.cumu.tail(1))
+    
+    def AR(self,trade_days):
+        return self.df.pnl.mean()*trade_days
+    
+    def Sharpe(self,trade_days):
+        return self.df.pnl.mean()/self.df.pnl.std()*math.sqrt(trade_days)
+    
+    def MDD(self):
+        return abs(min(self.df.dd))
+    
+    def Calmar(self,trade_days):
+        return self.AR(trade_days)/self.MDD()
 
 class price(perf):
     def __init__(self,df):
@@ -24,7 +55,6 @@ class price(perf):
         self.df['ma'] = self.df.adjclose.rolling(timeframe).mean()
         
         def ls_pos(row,diff):
-            print(row)
             if row.adjclose - row.ma > diff:
                 return 1  
             elif row.adjclose - row.ma < -diff:
@@ -47,25 +77,18 @@ class price(perf):
         else: 
             raise Exception("Invalid pos input")
         
-        super().perf_col(self.df)
+        self.Perf_col()
         
-        
-        # self.df['pos_xt1'] = self.df.pos.shift(1)
-        # self.df['pnl'] = self.df.pos_xt1* self.df.chg
-        # self.df['cumu'] = self.df.pnl.cumsum()
-        # self.df['dd'] = self.df.cumu - self.df.cumu.cummax()
         return self.df
 
-# TR = float(self.df.cumu.tail(1))
-# AR = self.df.pnl.mean()*365
-# Sharpe = self.df.pnl.mean()*365*math.sqrt(trade_days)
-# MDD = abs(min(self.df.dd))
-# Calmar = AR/MDD
+    
+
 
 start = time()
 df = pd.read_excel("data/processed/btcusd_20200510_20230310.xlsx")
 dfinstance = price(df)
 ma = dfinstance.MA(50,0.01,365,"ls")
-print(ma)
+print(dfinstance.df)
+print(dfinstance.Sharpe(365))
 end = time()
 print(end - start)
